4cc41aa525c8a11f3e99fb84a4d2c0b29a5cf9a3
Oz Elentok <oz.elen@gmail.com>
2025-02-26 11:16:02 +0200

    refactor(tests): Use doctstat subcases

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,14 +1,11 @@
+#include <cstdint>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 
 #include "Worker.hpp"
 #include "Server.hpp"
 
-int factorial(int number) {
-  return number <= 1 ? number : factorial(number - 1) * number;
-}
-
-TEST_CASE("Testing stat") {
+TEST_CASE("stat()") {
   auto host = "127.0.0.1";
   auto port = 5000;
   Server server(host, port);
@@ -17,19 +14,32 @@ TEST_CASE("Testing stat") {
   auto cmdr = server.get_commander(server.get_clients()[0]);
 
   struct stat file_info = {0};
-  int32_t ret = 0, worker_errno = 0;
-  std::tie(ret, worker_errno) = cmdr->getattr("/tmp", file_info);
-  CHECK(ret);
-  CHECK(worker_errno == 0);
-  CHECK((file_info.st_mode & S_IFDIR) == S_IFDIR);
+  bool ret = 0;
+  int32_t worker_errno = 0;
 
-  std::tie(ret, worker_errno) = cmdr->getattr("/DoesNotExist", file_info);
-  CHECK(!ret);
-  CHECK(worker_errno == ENOENT);
-  CHECK((file_info.st_mode & S_IFDIR) == S_IFDIR);
+  SUBCASE("dir exists") {
+    std::tie(ret, worker_errno) = cmdr->getattr("/tmp", file_info);
+    CHECK(ret);
+    CHECK(worker_errno == 0);
+    CHECK((file_info.st_mode & S_IFDIR) == S_IFDIR);
+  }
+
+  SUBCASE("does not exist and exists") {
+    std::tie(ret, worker_errno) = cmdr->getattr("/DoesNotExist", file_info);
+    CHECK(!ret);
+    CHECK(worker_errno == ENOENT);
+
+    std::tie(ret, worker_errno) = cmdr->getattr("/usr", file_info);
+    CHECK(ret);
+    CHECK(worker_errno == 0);
+    CHECK((file_info.st_mode & S_IFDIR) == S_IFDIR);
+  }
 
   worker.close();
   worker.wait();
 
-  REQUIRE_THROWS_WITH(cmdr->getattr("/tmp", file_info), "Disconnected");
+  SUBCASE("disconnected error") {
+    REQUIRE_THROWS_WITH(cmdr->getattr("/tmp", file_info), "Disconnected");
+    REQUIRE_THROWS_WITH(cmdr->getattr("/DoesNotExist", file_info), "Disconnected");
+  }
 }
