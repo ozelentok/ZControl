33b33ece4e47d9054df363420a1554f049a5a0c5
Oz Elentok <oz.elen@gmail.com>
2025-03-03 01:29:41 +0200

    tests(utils): Add retries on waiting for Commander to connect

--- tests/utils.hpp
+++ tests/utils.hpp
@@ -3,15 +3,32 @@
 #include "Worker.hpp"
 #include "Server.hpp"
 #include "doctest/doctest.h"
-#include <stdexcept>
+#include <chrono>
+#include <thread>
 #include <cstring>
 #include <sys/stat.h>
 
-#define SETUP_COMMANDER_WORKER                                                                                         \
-  auto host = "127.0.0.1";                                                                                             \
-  auto port = 5000;                                                                                                    \
-  Server server(host, port);                                                                                           \
-  Worker worker(host, port);                                                                                           \
-  auto cmdr = server.get_commander(server.get_clients()[0])
+using namespace std::chrono_literals;
 
-#define ERRNO_FMT(func_name, error_number) std::format(func_name "() errno {} {}", error_number, std::strerror(error_number))
+static constexpr auto MaxRetries = 5;
+static constexpr std::string Host = "127.0.0.1";
+static constexpr auto Port = 5000;
+static constexpr auto WaitTime = 20ms;
+
+#define SETUP_COMMANDER_WORKER                                                                                         \
+  Server server(Host, Port);                                                                                           \
+  Worker worker(Host, Port);                                                                                           \
+                                                                                                                       \
+  uint32_t retry_counter = 0;                                                                                          \
+  std::shared_ptr<Commander> cmdr = server.get_commander(server.get_clients()[0]);                                     \
+  while (cmdr == nullptr) {                                                                                            \
+    if (retry_counter >= MaxRetries) {                                                                                 \
+      REQUIRE_MESSAGE(cmdr != nullptr, "No Commander connected to worker");                                            \
+    }                                                                                                                  \
+    ++retry_counter;                                                                                                   \
+    std::this_thread::sleep_for(WaitTime);                                                                             \
+    cmdr = server.get_commander(server.get_clients()[0]);                                                              \
+  }
+
+#define ERRNO_FMT(func_name, error_number)                                                                             \
+  std::format(func_name "() errno {} {}", error_number, std::strerror(error_number))
